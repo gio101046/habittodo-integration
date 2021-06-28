36da3c274367f218cd38017dfd9b4669e1ae9104
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-06-28 18:59:25 -0400

    feat!: convert projects to .net 5 wip

---
+++ Habittodo.Model/ChecklistItem.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habittodo.Model
+{
+    public class ChecklistItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ Habittodo.Model/TodoHabitLink.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habittodo.Model
+{
+    public class TodoHabitLink : TableEntity
+    {
+        public TodoHabitLink() { }
+
+        public TodoHabitLink(string userId, string todoistId, string habiticaId, string todoistParentId = null)
+        {
+            PartitionKey = userId;
+            RowKey = todoistId;
+            TodoistParentId = todoistParentId;
+            HabiticaId = habiticaId;
+        }
+
+        public string TodoistParentId { get; set; }
+        public string HabiticaId { get; set; }
+
+        public HabitTodoLink Reverse(string habiticaParentId = null)
+        {
+            return new HabitTodoLink(PartitionKey, HabiticaId, RowKey, habiticaParentId);
+        }
+    }
+}
